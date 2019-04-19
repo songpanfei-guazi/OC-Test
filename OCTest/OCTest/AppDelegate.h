27e5da92f9b2cfacc74820bbffbf2d18e69d22c0
songpanfei <songpanfei@guazi.com>
2019-04-19 10:48:17 +0800

    afn3.2

---
+++ OCTest/OCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCTest
+//
+//  Created by songpanfei on 2019/4/19.
+//  Copyright © 2019 chesupai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCTest/OCTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCTest
+//
+//  Created by songpanfei on 2019/4/19.
+//  Copyright © 2019 chesupai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
